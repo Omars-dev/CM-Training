f03974e69616f12bc3fc59e0aef4797ea17f28b0
Omar Faruk <omarfaruks2018@gmail.com>
2023-08-02 16:52:19 +0600

    update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -29,13 +29,13 @@
 
                         <div class="latest-tweet">
                             <div class="media">
-                                <ul id="menu-best-services" class="menu"><li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="{{url('/course-details')}}"><i class="fa fa-angle-double-right"></i>Freelancing & Outsourcing</a></li>
+                                <ul id="menu-best-services" class="menu"><li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="{{url('/course-details')}}"><i class="fa fa-angle-double-right"></i> Freelancing & Outsourcing</a></li>
                                     <li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="{{url('/hardware-and-networking')}}"><i class="fa fa-angle-double-right"></i> Hardware & Networking</a></li>
-                                    <li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="{{url('/routing-and-switching')}}"><i class="fa fa-angle-double-right"></i>Routing & Switching</a></li>
+                                    <li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="{{url('/routing-and-switching')}}"><i class="fa fa-angle-double-right"></i> Routing & Switching</a></li>
                                     <li id="menu-item-157" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-157"><a href="{{url('/network-security')}}"><i class="fa fa-angle-double-right"></i> Network Security</a></li>
                                     <li id="menu-item-158" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-158"><a href="{{url('/system-administration')}}"><i class="fa fa-angle-double-right"></i> System Administration</a></li>
                                     <li id="menu-item-159" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-159"><a href="{{url('/web-development')}}"><i class="fa fa-angle-double-right"></i> Website Design</a></li>
-                                    <li id="menu-item-160" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-160"><a href="{{url('/web-development')}}"><i class="fa fa-angle-double-right"></i>Website Development</a></li>
+                                    <li id="menu-item-160" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-160"><a href="{{url('/web-development')}}"><i class="fa fa-angle-double-right"></i> Website Development</a></li>
                                     <li id="menu-item-163" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-163"><a href="{{url('/software-development')}}"><i class="fa fa-angle-double-right"></i> Software Developmentn</a></li>
                                     <li id="menu-item-161" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-161"><a href="{{url('/cloud-visualization')}}"><i class="fa fa-angle-double-right"></i> Cloud & Virtualization </a></li>
                                 </ul>
@@ -50,12 +50,12 @@
                             </h3>
                         </div>
                         <div class="subscription">
-                            <ul id="menu-best-services" class="menu"><li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="#"><i class="fa fa-angle-double-right"></i>Computer Technology</a></li>
-                                <li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="'/civil-and-architechture')}}"><i class="fa fa-angle-double-right"></i>Civil & Architecture Technology</a></li>
+                            <ul id="menu-best-services" class="menu"><li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="#"><i class="fa fa-angle-double-right"></i> Computer Technology</a></li>
+                                <li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="'/civil-and-architechture')}}"><i class="fa fa-angle-double-right"></i> Civil & Architecture Technology</a></li>
                                 <li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href=""><i class="fa fa-angle-double-right"></i> Electrical Technology</a></li>
-                                <li id="menu-item-157" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-157"><a href=""><i class="fa fa-angle-double-right"></i>Mechanical Technology</a></li>
-                                <li id="menu-item-158" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-158"><a href=""><i class="fa fa-angle-double-right"></i>Power Technology</a></li>
-                                <li id="menu-item-159" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-159"><a href=""><i class="fa fa-angle-double-right"></i>Electronics Technology</a></li>
+                                <li id="menu-item-157" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-157"><a href=""><i class="fa fa-angle-double-right"></i> Mechanical Technology</a></li>
+                                <li id="menu-item-158" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-158"><a href=""><i class="fa fa-angle-double-right"></i> Power Technology</a></li>
+                                <li id="menu-item-159" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-159"><a href=""><i class="fa fa-angle-double-right"></i> Electronics Technology</a></li>
                                 <li id="menu-item-160" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-160"><a href="#"><i class="fa fa-angle-double-right"></i> Telecommunication Technology</a></li>
                                 </ul>
 
@@ -65,55 +65,16 @@
                     <div class="col-md-3">
                         <div class="section-heading-2">
                             <h3 class="section-title">
-                                <span>Recent Projects</span>
+                                <span>New Courses</span>
                             </h3>
                         </div>
-
-                        <div class="flickr-widget">
-                            <ul class="flickr-list">
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-1">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-2">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-3">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-4">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-5">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/img6.jpg" data-l')}}ightbox="picture-6">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-7">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{url('frontend/images/portfolio/cmit-gallery.png')}}" data-lightbox="picture-8">
-                                        <img src="{{url('frontend/images/portfolio/cmit-gallery.png')}}" alt="" class="img-responsive">
-                                    </a>
-                                </li>
+                        <div class="subscription">
+                            <ul id="menu-best-services" class="menu"><li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="#"><i class="fa fa-angle-double-right"></i> Python Bootcamp</a></li>
+                                <li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="#"><i class="fa fa-angle-double-right"></i> Django Zero to Hero</a></li>
+                                <li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="#"><i class="fa fa-angle-double-right"></i> MERN Stack</a></li>
                             </ul>
                         </div>
-                    </div><!--/.col-md-3 -->
+                    </div>
                 </div><!--/.row -->
             </div><!-- /.container -->
         </section>
